2a449e72a980d3bd45c961a1e73f2a4bcc2a7493
JarkZhang <includezzi@126.com>
2019-04-19 00:27:03 +0800

    revert

--- HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
+++ HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
@@ -22,7 +22,11 @@ using Game.View.Panels;
 <<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
 <<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
 <<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
+<<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
 using System.IO;
+=======
+using Game.Common;
+>>>>>>> parent of f93c798... 提供些辅助功能，详见石墨文档:HoldiayProjectHighFive/Assets/Game/Control/Person/Persons/Player.cs
 =======
 using Game.Common;
 >>>>>>> parent of f93c798... 提供些辅助功能，详见石墨文档:HoldiayProjectHighFive/Assets/Game/Control/Person/Persons/Player.cs
@@ -46,6 +50,7 @@ namespace Game.Control.Person
     [Serializable]
     public class Player : AbstractPerson, IXmlSerializable
     {
+<<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
 <<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
 <<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
 <<<<<<< HEAD:HoldiayProjectHighFive/Assets/Game/Control/Person/Player.cs
@@ -84,6 +89,9 @@ namespace Game.Control.Person
             }
 
         }
+=======
+
+>>>>>>> parent of f93c798... 提供些辅助功能，详见石墨文档:HoldiayProjectHighFive/Assets/Game/Control/Person/Persons/Player.cs
 =======
 
 >>>>>>> parent of f93c798... 提供些辅助功能，详见石墨文档:HoldiayProjectHighFive/Assets/Game/Control/Person/Persons/Player.cs
